4862a60c39cdb6025e3abf26dfafc87cfaaa5596
nibedita6302 <nibedita.6302@gmail.com>
2023-11-15 02:06:27 +0530

    Major Changes - Change upload folder to static folder, app.py and home.html

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, flash, request, redirect, url_for, render_template, send_file
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import tensorflow as tf
-from tensorflow.keras.models import Model, load_model
 import os
 import cv2
 
@@ -10,11 +9,8 @@ import cv2
 app = Flask(__name__)
 app.debug = True
 CORS(app)
-
-
-UPLOAD_FOLDER = 'upload/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = 'jpg'
+app.config['STATIC_FOLDER'] = "static/"
+ALLOWED_EXTENSIONS = ['jpeg']
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -22,56 +18,60 @@ def allowed_file(filename):
 # Load model
 model = tf.keras.models.load_model(r'.\save_cnn_model_1o4_batch=8_lr=0.001\save_cnn_model_1o4_batch=8_lr=0.001', compile=False)
 
-@app.route('/', methods=['GET']) 
+@app.route('/', methods=['GET', 'POST']) 
 def Hello():
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            data = jsonify({
+                "status": "error",
+                "status_code": 404,
+                "message": "Upload an image first"
+            })
+            return render_template('home.html', data=data)
+        file = request.files['image']
+        if file.filename == '':
+            data = jsonify({
+                "status": "error",
+                "status_code": 404,
+                "message": "No image selected for uploading"
+            })
+            return render_template('home.html', data=data)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # Save image 
+            savePath = os.path.join(app.config['STATIC_FOLDER'], filename)
+            file.save(savePath)
+            return redirect(url_for('Predict', path=savePath))
+        else:
+            data = jsonify({
+                "status": "error",
+                "status_code": 404,
+                "message": "Unsupported image type"
+            })
+            return render_template('home.html', data=data)
     return render_template('home.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def Predict():
-    if 'image' not in request.files:
-        return {
-            "status": "error",
-            "status_code": 404,
-            "message": "Upload an image first"
-        }
-    file = request.files['image']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return {
-            "status": "error",
-            "status_code": 404,
-            "message": "No image selected for uploading"
-        }
-
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-
-        # Save image 
-        savePath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(savePath)
-        # Read image
-        img = cv2.imread(savePath)
-        x_true = tf.convert_to_tensor(img, tf.float32)
-        x_true = tf.expand_dims(x_true, axis=0)
-        #print(img.shape)
-        # Normalize image
-        x_true = x_true / 255.0
-        # prediction
-        y_pred = model.predict(x_true)
-        y_pred_arg = tf.math.argmax(y_pred, axis=-1) ## argmax
-        return {
-            'y_pred':y_pred,
-            'y_pred_arg':y_pred_arg,
-            'status_code': 200
-        }
-    
-    else:
-        return {
-            "status": "error",
-            "status_code": 415,
-            "message": "Unsupported Media Type, server cannot process the request body."
-        }
+    path = request.args.get('path')
+    # Read image
+    img = cv2.imread(path)
+    x_true = tf.convert_to_tensor(img, tf.float32)
+    x_true = tf.expand_dims(x_true, axis=0)
+    # Normalize image
+    x_true = x_true / 255.0 
+    # prediction
+    y_pred = model.predict(x_true)
+    y_pred_arg = tf.math.argmax(y_pred, axis=-1) ## argmax
+    func = lambda x: round(x*100,2)
+    data = {
+        "prediction":enumerate(map(func, y_pred[0])),
+        "status_code": 200,
+        "path": path,
+        "result": int(y_pred_arg[0]+1)
+    }
+    return render_template('home.html', data=data)
 
 
 if __name__ == '__main__':
